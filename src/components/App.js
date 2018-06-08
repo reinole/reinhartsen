34ae44b5ef756a7072da5c0e14b8df9662d4f4a2
Ole christian Reinhartsen <reinhartsen.oc@gmail.com>
2018-06-08 19:58:45 +0200

    Ready for work

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Menu from './Menu';
+//import Menu from './Menu';
+import { ThemeProvider } from 'styled-components';
+import colors from '../utils/colors';
 import '../App.css';
 import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <div className="App">
-            <Menu />
-            <Route exact path="/" component={Home} />
-          </div>
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={colors}>
+        <BrowserRouter>
+          <Switch>
+            <div className="App">
+              {/* <Menu /> */}
+              <Route exact path="/" component={Home} />
+            </div>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     );
   }
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 import ProfileImage from '../utils/images/Ole-christian-ludens.jpg';
 
 const MenuBar = styled.div`
@@ -32,20 +31,6 @@ const Occupation = styled.p`
   color: ${props => props.theme.light};
 `;
 
-const MenuHome = styled(NavLink)`
-  padding: 1rem 0;
-  color: ${props => props.theme.light};
-  width: 100%;
-  text-align: center;
-
-  &:hover,
-  &:focus {
-    cursor: pointer;
-    background-color: ${props => props.theme.orange};
-    text-decoration: underline;
-  }
-`;
-
 export default class Menu extends Component {
   state = {};
 
@@ -55,10 +40,6 @@ export default class Menu extends Component {
         <Name>Ole Christian Reinhartsen</Name>
         <Occupation>Webutvikler</Occupation>
         <Image src={ProfileImage} />
-        <MenuHome to="/Home">Home</MenuHome>
-        <MenuHome>About me</MenuHome>
-        <MenuHome>Experience</MenuHome>
-        <MenuHome>Projects</MenuHome>
       </MenuBar>
     );
   }
